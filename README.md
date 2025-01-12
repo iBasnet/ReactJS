97e3fe44ad0019382240fe60393e6dc5719158a2
naive <iampratikbasnet@gmail.com>
2025-01-12 21:37:18 +0545

    caching is auto now

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Navie - Responsive Navbar 💡
+# React + Vite
 
-A responsive navbar built with React.js for seamless navigation across devices.
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-[✨Live Demo✨](https://navie.vercel.app)
+Currently, two official plugins are available:
 
----
-
-By [Pratik Basnet](https://pratikbasnet.com)
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,102 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+
+const fetchFromGitHub = async (username) => {
+
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    if (!response.ok) {
+      throw new Error("Response ain't okay")
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const { name, login, location, avatar_url } = data;
+
+    return { name, login, location, avatar_url };
+
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
 
 export default function App() {
 
-  function toggleClass() {
-    const navHeader = document.querySelector('header');
-    navHeader.classList.toggle('active');
+  const [username, setUsername] = useState();
+  const [count, setCount] = useState(0);
+  const [userData, setUserData] = useState(null);
+  const [degree, setDegree] = useState(0);
 
-    const boxIcon = document.querySelector('i');
-    boxIcon.classList.toggle('bx-x');
-  }
+  useEffect(() => {
+
+    async function fetchSetter() {
+
+      if (username) {
+        const data = await fetchFromGitHub(username);
+        // You need to await the resolved value of fetchFromGitHub and then pass the actual data to setUserData.
+        // Otherwise the promise will be unfilled if don't await from the changes
+        setUserData(data)
+      }
+    }
+
+    fetchSetter();
+  }, [username]);
+
+  useEffect(() => {
+    if (!username) {
+      const ask = prompt("Search for a username on GitHub:", "iBasnet");
+      setUsername(ask);
+    }
+  }, [username]);
 
   return (
-    <header>
-      <nav>
-        <div className="logo">
-          <h1>EÐ</h1>
+    !username
+      ? (
+        <p>Search for a username on GitHub.</p>
+      )
+      : (
+        <main>
+          <Counter count={count} setCount={setCount} setDegree={setDegree} />
+          <Profile userData={userData} degree={degree} />
+        </main >
+      )
+  )
+}
+
+function Counter({ count, setCount, setDegree }) {
+  return (
+    <section className="counter">
+      <h1>Caching in JavaScript</h1>
+      <h2>Current Count: {count}</h2>
+      <button
+        title='just click me'
+        onClick={() => {
+          setCount(prev => prev + 1);
+          setDegree(prev => prev + 36);
+        }
+        }>Click Me</button>
+    </section >
+  )
+}
+
+function Profile({ userData, degree }) {
+  return (
+    userData && (
+      <section className='profile'>
+        <div>
+          <img src={userData.avatar_url} alt="Profile Picture"
+            style={{ rotate: `${degree}deg` }}
+          />
         </div>
-        <ul>
-          <li className="active">Home</li>
-          <li>Products</li>
-          <li>Services</li>
-          <li>Social</li>
-          <li>Contact</li>
-        </ul>
-        <div className="hamburger" onClick={toggleClass}>
-          <i className='bx bx-menu bx-lg'></i>
+        <div>
+          <h1>{userData.name}</h1>
+          <p>@{userData.login}</p>
+          <p>{userData.location}</p>
         </div>
-        <div className="btn-group">
-          <a href="">Sign up</a>
-          <button><a href="">Log in</a></button>
-        </div>
-      </nav>
-    </header>
-  );
+      </section>
+    )
+  )
 }
